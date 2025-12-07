13051697acabd4c9bccd6bfb1979090d603550fc
youjuanshen <128175129+youjuanshen@users.noreply.github.com>
2025-12-07 13:31:11 +0800

    Create u1_l1.js

---
+++ data/speaking/u1_l1.js
@@ -0,0 +1,9 @@
+window.LOAD_QUIZ({
+    title: "Unit 1 Lesson 1: Nice to meet you (口语)",
+    mode: "speaking",
+    questions: [
+        { qNum: 1, text: "看图说词", image: "img/apple.png", guide: "Apple" },
+        { qNum: 2, text: "扮演 Miss Gao 打招呼", image: "img/miss_gao.png", guide: "Hello, I'm Miss Gao." },
+        { qNum: 3, text: "听到 Nice to meet you 怎么回答？", guide: "Nice to meet you, too." }
+    ]
+});
