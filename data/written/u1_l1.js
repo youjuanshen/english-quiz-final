a2150f75fc1cac2b044f8497ae200eab1815dd1f
youjuanshen <128175129+youjuanshen@users.noreply.github.com>
2025-12-07 13:31:48 +0800

    Create u1_l1.js

---
+++ data/written/u1_l1.js
@@ -0,0 +1,10 @@
+window.LOAD_QUIZ({
+    title: "Unit 1 Lesson 1: Nice to meet you (笔试)",
+    mode: "written",
+    timeLimit: 300,
+    questions: [
+        { qNum: 1, type: "select", text: "选出 Apple", options: ["img/banana.png", "img/apple.png"], correct: "img/apple.png" },
+        { qNum: 2, type: "select", text: "Nice to meet you 中文是？", audio: "Nice to meet you", options: ["你好", "很高兴见到你"], correct: "很高兴见到你" },
+        { qNum: 3, type: "drag", text: "连词成句", words: ["Hello", "I", "am", "Ben"], correct: "Hello I am Ben" }
+    ]
+});
